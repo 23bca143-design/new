0f9aef699e6b0ac5b2e7f1982145b1255a48c6dc
23bca143-design <23bca143@sxca.edu.in>
2026-03-28 21:49:44 +0530

    Initial upload: Cross Browser Compatibility Chatbot
    This project includes(Python coding) for the chatbot development. It contains the main app script, knowledge base text file, and basic front-end HTML.

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify, render_template
+from groq import Groq
+import os
+
+app = Flask(__name__)
+
+GROQ_API_KEY = "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"
+client = Groq(api_key=GROQ_API_KEY)
+
+try:
+    with open("knowledge.txt", "r", encoding="utf-8") as file:
+        knowledge = file.read()
+except:
+    knowledge = ""
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    try:
+        data = request.get_json()
+        user_message = data["message"]
+        msg = user_message.lower().strip()
+
+        greetings = ["hi", "hello", "hey", "hii", "helo"]
+
+        if msg in greetings:
+            return jsonify({
+                "reply": "Hello! 😊 I can help you with Cross Browser Compatibility. Ask me anything related to it."
+            })
+        
+        completion = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"""
+You are an expert chatbot ONLY for Cross Browser Compatibility.
+
+Rules:
+- If question is related → answer properly.
+- If NOT related → say:
+  "I can only assist with Cross Browser Compatibility topics."
+
+Be polite and friendly.
+
+Knowledge:
+{knowledge}
+"""
+                },
+                {
+                    "role": "user",
+                    "content": user_message
+                }
+            ],
+        )
+
+        reply = completion.choices[0].message.content
+        return jsonify({"reply": reply})
+
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({"reply": "Server error"}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
